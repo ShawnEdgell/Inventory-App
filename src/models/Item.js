05a1afa1e454419f1364f0cfdf04cda6c275e92f
shawn edgell <shawn_edgell@hotmail.com>
2023-11-06 13:39:51 -0500

    file directory setup

---
+++ src/models/Item.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  imageUrl: String,
+}, { timestamps: true });
+
+module.exports = mongoose.models.Item || mongoose.model('Item', itemSchema);
